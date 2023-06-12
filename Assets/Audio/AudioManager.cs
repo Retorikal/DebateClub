c7fa4f507199705f63d5a7adb136b758c7333e92
Ardha <ardha.theo@gmail.com>
2023-06-13 01:17:28 +0700

    Implement Audio

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private EventReference MusicSound;
+    [SerializeField] private EventReference SpeechSound;
     public static AudioManager instance { get; private set; }
-
+    
     private void Awake()
     {
         if (instance != null)
@@ -15,10 +18,12 @@ public class AudioManager : MonoBehaviour
         }
 
         instance = this;
+        AudioManager.instance.PlayOneShot(MusicSound, this.transform.position);
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
     }
+
 }
